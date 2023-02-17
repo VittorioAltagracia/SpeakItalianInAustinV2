bfcd4e2a18ccb80ff446eeba780d2c09123980f3
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-02-16 23:39:20 -0600

    Implemented carousel, styling and positiong, experimented with JS

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// const photosItVer = document.querySelector(".testing");
+// console.log(photosItVer);
+// const chiSiamo = document.querySelector(".chi-siamo");
+// console.log(chiSiamo);
+
+// const test = document.querySelector(".test");
+
+// chiSiamo.addEventListener("click", function () {
+//   photosItVer.textContent = "Chi siamo";
+// });
