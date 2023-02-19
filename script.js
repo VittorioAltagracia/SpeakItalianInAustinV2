28ee76c8b503fdd5ea71f621a3de0fcbc77cfd29
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-02-19 00:28:45 -0600

    Improved layout, removed photos page and added some JS

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 "use strict";
 
-// Selecting elements on IndexIT
+// Selecting elements on Index Italian version
 const chiSiamo = document.querySelector(".chi-siamo");
+const carouselIt = document.getElementById("showCarIt");
 
-// Selecting elements on Photos
-
+// Selecting elements on Index English version
 const lookAtUs = document.querySelector(".lookAtUs");
+const carouselEn = document.getElementById("showCarEn");
 
-let page = document.body.id;
+// Variable that keeps track of the language on the page
+let language = document.body.id;
 
-if (page === "it") {
-  console.log("it");
-} else {
-  console.log("english");
+switch (language) {
+  case "it":
+    chiSiamo.addEventListener("click", function () {
+      carouselIt.classList.toggle("hidden");
+    });
+    break;
+
+  case "en":
+    lookAtUs.addEventListener("click", function () {
+      carouselEn.classList.toggle("hidden");
+    });
+    break;
 }
