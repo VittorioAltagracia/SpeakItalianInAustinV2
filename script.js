901f8492ab86886b8c7ac4bf895201254743e5df
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-02-18 22:51:12 -0600

    test commit

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 "use strict";
 
 // Selecting elements on IndexIT
-// const chiSiamo = document.querySelector(".chi-siamo");
+const chiSiamo = document.querySelector(".chi-siamo");
 
-// // Selecting elements on Photos
+// Selecting elements on Photos
 
-// const lookAtUs = document.querySelector(".lookAtUs");
+const lookAtUs = document.querySelector(".lookAtUs");
 
-// let page = document.body.id;
+let page = document.body.id;
 
-// if (page === "it") {
-//   console.log("it");
-// } else {
-//   console.log("english");
-// }
+if (page === "it") {
+  console.log("it");
+} else {
+  console.log("english");
+}
