01705765d9c9683b9f79c0c9e730f0f77fe4dd64
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-02-26 18:02:34 -0600

    language refactoring in progress

--- js/data.js
+++ js/data.js
@@ -7,11 +7,35 @@ const content = {
     en: "Speak Italian in Austin!",
     it: "Parliamo Italiano ad Austin!",
   },
-  homeLink: {
+  header1: {
     en: "Join us for our monthly events!",
     it: "Unisciti a noi ai nostri eventi mensili!",
   },
-  photoLink: {
+  header2: {
+    en: "Why learn Italian?",
+    it: "Per ché imparare l'italiano?",
+  },
+  nav1: {
+    en: " Home",
+    it: " Home",
+  },
+  nav2: {
+    en: " Look at us",
+    it: " Chi siamo",
+  },
+  nav3: {
+    en: " What we do",
+    it: " Cosa facciamo",
+  },
+  nav4: {
+    en: " Languages",
+    it: " Lingue",
+  },
+  nav5: {
+    en: " Contact us",
+    it: " Contattaci",
+  },
+  contactLink: {
     en: "",
     it: "",
   },
