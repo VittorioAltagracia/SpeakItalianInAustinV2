5a766094a78a9c062b2416274c609d61491b08d6
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-02-26 18:41:19 -0600

    major update to data.js file

--- js/data.js
+++ js/data.js
@@ -7,14 +7,6 @@ const content = {
     en: "Speak Italian in Austin!",
     it: "Parliamo Italiano ad Austin!",
   },
-  header1: {
-    en: "Join us for our monthly events!",
-    it: "Unisciti a noi ai nostri eventi mensili!",
-  },
-  header2: {
-    en: "Why learn Italian?",
-    it: "Per ché imparare l'italiano?",
-  },
   nav1: {
     en: " Home",
     it: " Home",
@@ -35,17 +27,37 @@ const content = {
     en: " Contact us",
     it: " Contattaci",
   },
-  contactLink: {
-    en: "",
-    it: "",
+  header1: {
+    en: "Join us for our monthly events!",
+    it: "Unisciti a noi ai nostri eventi mensili!",
   },
-  aboutLink: {
-    en: "",
-    it: "",
+  header2: {
+    en: "Why learn Italian?",
+    it: "Per ché imparare l'italiano?",
   },
-  langLink: {
-    en: "",
-    it: "",
+  par1: {
+    en: `Who are we? Lorem ipsum dolor sit amet consectetur
+    adipisicing elit. Laudantium soluta quis, illo rerum ad
+    necessitatibus fuga adipisci? Voluptates commodi at amet
+    aspernatur, sint porro pariatur optio, nostrum deleniti, illo rem.`,
+    it: `Chi siamo? Lorem ipsum dolor sit amet consectetur
+    adipisicing elit. Laudantium soluta quis, illo rerum ad
+    necessitatibus fuga adipisci? Voluptates commodi at amet
+    aspernatur, sint porro pariatur optio, nostrum deleniti, illo rem.`,
+  },
+  par2: {
+    en: `Because it's one of the best investments you can possibly make in
+    yourself. Lorem ipsum dolor, sit amet consectetur adipisicing
+    elit. Quis eveniet architecto dolores excepturi pariatur veritatis
+    reprehenderit sit, enim repellendus, voluptates dolor,
+    exercitationem voluptatem ullam laborum eius consectetur. Illum,
+    et reprehenderit!`,
+    it: `Perche è uno degli investimenti migliori che puoi fare su te
+    stesso. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+    Quis eveniet architecto dolores excepturi pariatur veritatis
+    reprehenderit sit, enim repellendus, voluptates dolor,
+    exercitationem voluptatem ullam laborum eius consectetur. Illum,
+    et reprehenderit!`,
   },
   paragraph1Title: {
     en: "",
